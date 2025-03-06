870b4f9dd9b63b1d6e03612e51bd45d3ecc74e65
Velu <velu@umich.edu>
2025-03-06 10:49:49 -0600

    ip address is not hard coded and not sending processed data

--- VideoStreamingClient/VideoStreamingClient/IMUClient.swift
+++ VideoStreamingClient/VideoStreamingClient/IMUClient.swift
@@ -32,32 +32,60 @@ class IMUClient {
         motionManager.startMagnetometerUpdates()
         motionManager.startDeviceMotionUpdates()
         
+        
         // Set up a timer to send data at regular intervals (every 0.1 seconds for example)
         dataTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(sendIMUData), userInfo: nil, repeats: true)
     }
     
     @objc func sendIMUData() {
         // Get data from sensors
-        if let accData = motionManager.accelerometerData?.acceleration,
-           let gyroData = motionManager.gyroData?.rotationRate,
-           let magData = motionManager.magnetometerData?.magneticField,
-           let quatData = motionManager.deviceMotion?.attitude.quaternion {
+        if let rawAccData = motionManager.accelerometerData?.acceleration,
+           let rawGyroData = motionManager.gyroData?.rotationRate,
+           let rawMagData = motionManager.magnetometerData?.magneticField,
+           
+           let accData = motionManager.deviceMotion?.userAcceleration,
+           let rotData = motionManager.deviceMotion?.rotationRate,
+           let magData = motionManager.deviceMotion?.magneticField,
+           let orientationData = motionManager.deviceMotion?.attitude,
+           let quatData = motionManager.deviceMotion?.attitude.quaternion{
             
             let timestamp = CACurrentMediaTime()  // Timestamp in seconds
             let startMarker = Data([0xAB, 0xCD, 0xEF, 0x02])  // 4-byte marker (unique for IMU data)
+            
             let timestampData = withUnsafeBytes(of: timestamp) { Data($0) }
             
-            let accXData = withUnsafeBytes(of: accData.x) { Data($0) }
-            let accYData = withUnsafeBytes(of: accData.y) { Data($0) }
-            let accZData = withUnsafeBytes(of: accData.z) { Data($0) }
+            let rawAccX = withUnsafeBytes(of: rawAccData.x) { Data($0) }
+            let rawAccY = withUnsafeBytes(of: rawAccData.y) { Data($0) }
+            let rawAccZ = withUnsafeBytes(of: rawAccData.z) { Data($0) }
             
-            let gyroXData = withUnsafeBytes(of: gyroData.x) { Data($0) }
-            let gyroYData = withUnsafeBytes(of: gyroData.y) { Data($0) }
-            let gyroZData = withUnsafeBytes(of: gyroData.z) { Data($0) }
+            let rawGyroX = withUnsafeBytes(of: rawGyroData.x) { Data($0) }
+            let rawGyroY = withUnsafeBytes(of: rawGyroData.y) { Data($0) }
+            let rawGyroZ = withUnsafeBytes(of: rawGyroData.z) { Data($0) }
             
-            let magXData = withUnsafeBytes(of: magData.x) { Data($0) }
-            let magYData = withUnsafeBytes(of: magData.y) { Data($0) }
-            let magZData = withUnsafeBytes(of: magData.z) { Data($0) }
+            let rawMagX = withUnsafeBytes(of: rawMagData.x) { Data($0) }
+            let rawMagY = withUnsafeBytes(of: rawMagData.y) { Data($0) }
+            let rawMagZ = withUnsafeBytes(of: rawMagData.z) { Data($0) }
+            
+            
+            
+            let accDataX = withUnsafeBytes(of: accData.x) { Data($0) }
+            let accDataY = withUnsafeBytes(of: accData.y) { Data($0) }
+            let accDataZ = withUnsafeBytes(of: accData.z) { Data($0) }
+            
+            let rotDataX = withUnsafeBytes(of: rotData.x) { Data($0) }
+            let rotDataY = withUnsafeBytes(of: rotData.y) { Data($0) }
+            let rotDataZ = withUnsafeBytes(of: rotData.z) { Data($0) }
+            
+            
+            let magDataX = withUnsafeBytes(of: magData.field.x) { Data($0) }
+            let magDataY = withUnsafeBytes(of: magData.field.y) { Data($0) }
+            let magDataZ = withUnsafeBytes(of: magData.field.z) { Data($0) }
+            
+                        
+            let yaw = withUnsafeBytes(of: orientationData.yaw) { Data($0) }
+            let pitch = withUnsafeBytes(of: orientationData.pitch) { Data($0) }
+            let roll = withUnsafeBytes(of: orientationData.roll) { Data($0) }
+
             
             let quatXData = withUnsafeBytes(of: quatData.x) { Data($0) }
             let quatYData = withUnsafeBytes(of: quatData.y) { Data($0) }
@@ -68,10 +96,15 @@ class IMUClient {
             
             
             // Combine all data into a single packet
-            let packet = startMarker + timestampData +
-            accXData + accYData + accZData +
-            gyroXData + gyroYData + gyroZData +
-            magXData + magYData + magZData +
+            let packet =
+            startMarker + timestampData +
+            rawAccX + rawAccY + rawAccZ +
+            rawGyroX + rawGyroY + rawGyroZ +
+            rawMagX + rawMagY + rawMagZ +
+            accDataX + accDataY + accDataZ +
+            rotDataX + rotDataY + rotDataZ +
+            magDataX + magDataY + magDataZ +
+            yaw + pitch + roll +
             quatXData + quatYData + quatZData + quatWData
             
             tcpClient.send(data: packet)
