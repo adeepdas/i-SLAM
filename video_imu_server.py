3b0c1255d72481cec14c34b74c37c06e1484a250
Velu Manohar <velumanohar2000@gmail.com>
2025-04-07 17:05:57 -0400

    depth, video, intrinsics working

--- video_imu_server.py
+++ video_imu_server.py
@@ -8,13 +8,15 @@ import queue
 
 
 START_MARKER = b'\xAB\xCD\xEF\x01'  # 4-byte unique identifier
-HEADER_SIZE = 16  # 4 (marker) + 8 (timestamp) + 4 (frame size)
+HEADER_SIZE = 24  # 4 (marker) + 8 (timestamp) + 4 (video size) + 4 (depth size) + 4 (intrinsic Size) 
 IMU_PACKET_SIZE = 212 # 4 (marker) + 8 (timestamp) + 200 (data)
 
 # Server Config
 VIDEO_PORT = 12005
 IMU_PORT = 13005
 DEPTH_PORT = 14005
+
+ALL_DATA_PORT = 25005
 
 DEPTH_HEIGHT = 180
 DEPTH_WIDTH = 320
@@ -42,18 +44,30 @@ def extract_packet(buffer):
 
     # Extract header information
     header = buffer[marker_index:marker_index + HEADER_SIZE]
-    timestamp, frame_size = struct.unpack("dI", header[4:16])
+    timestamp, video_size, depth_size, intrinsic_size = struct.unpack("dIII", header[4:HEADER_SIZE])
+    frame_size = video_size + depth_size + intrinsic_size
 
 
     # Check if the entire frame data is available
     if len(buffer) < marker_index + HEADER_SIZE + frame_size:
         return None, buffer  # Incomplete frame data
     
+    # Extract video data
+    video_data = buffer[marker_index + HEADER_SIZE:marker_index + HEADER_SIZE + video_size]
+
+    # Extract depth data
+    depth_data = buffer[marker_index + HEADER_SIZE + video_size:marker_index + HEADER_SIZE + video_size + depth_size]
+
+    # Extract intrinsic data
+    intrinsic_buffer = buffer[marker_index + HEADER_SIZE + video_size + depth_size:marker_index + HEADER_SIZE + frame_size]
+
+    fx, fy, cx, cy = struct.unpack("ffff", intrinsic_buffer) 
+
     # Extract the frame data
-    frame_data = buffer[marker_index + HEADER_SIZE:marker_index + HEADER_SIZE + frame_size]
+    # frame_data = buffer[marker_index + HEADER_SIZE:marker_index + HEADER_SIZE + frame_size]
     remaining_buffer = buffer[marker_index + HEADER_SIZE + frame_size:]
 
-    return (timestamp, frame_data), remaining_buffer
+    return (timestamp, video_data, depth_data, fx, fy, cx, cy), remaining_buffer
 
 
 def receive_h264_video(host, port):
@@ -222,6 +236,70 @@ def display_loop():
 
     cv2.destroyAllWindows()
 
+
+def receive_data(host, port):
+    # Create and bind a TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    
+    print(f"Starting server on {host}:{port}, waiting for a connection...")
+    connection, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
+
+    # Initialize H264 decoder
+    decoder = h264decoder.H264Decoder()
+
+    # Buffer to accumulate received data
+    buffer = b''
+
+    try:
+        while True:
+            # Receive data in chunks
+            data_in = connection.recv(4096)
+            if not data_in:
+                break
+            buffer += data_in
+
+            while True:
+                extracted_packet, buffer = extract_packet(buffer)
+                if extracted_packet is None:
+                    break  # Wait for more data
+
+                timestamp, video_data, depth_data, fx, fy, cx, cy = extracted_packet
+
+                # print fx, fy, cx, cy
+                print(f"Intrinsic: fx={fx}, fy={fy}, cx={cx}, cy={cy}")
+
+
+
+                # Display depth data
+                depth_img = buffer_to_image(depth_data)
+                if not depth_frame_queue.full():
+                    depth_frame_queue.put(depth_img)
+
+                # Decode H264 frame and display
+                framedatas = decoder.decode(video_data)
+                for framedata in framedatas:
+                    (frame, w, h, ls) = framedata
+                    if frame is not None:
+                        # Convert the frame to a numpy array
+                        frame = np.frombuffer(frame, dtype=np.ubyte).reshape((h, ls // 3, 3))
+                        if not video_frame_queue.full():
+                            video_frame_queue.put(frame)                        
+
+                    else:
+                        print('frame is None')
+                        break
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        connection.close()
+        cv2.destroyAllWindows()
+        print("Connection closed")
+
+
 if __name__ == "__main__":
     # get ip address
     ip_address = get_local_ip()
@@ -232,19 +310,22 @@ if __name__ == "__main__":
     imu_thread = threading.Thread(target=receive_imu_data, args=(HOST, IMU_PORT))
     depth_thread = threading.Thread(target=receive_depth_data, args=(HOST, DEPTH_PORT))
 
+    receive_data_thread = threading.Thread(target=receive_data, args=(HOST, ALL_DATA_PORT))
+
     # Start both threads
-    video_thread.start()
-    imu_thread.start()
-    depth_thread.start()
+    # video_thread.start()
+    # imu_thread.start()
+    # depth_thread.start()
+    receive_data_thread.start()
 
     # Main thread handles GUI
     display_loop()
 
     # Wait for both to finish
-    video_thread.join()
-    imu_thread.join()
-    depth_thread.join()
-
+    # video_thread.join()
+    # imu_thread.join()
+    # depth_thread.join()
+    receive_data_thread.join()
     print("Server shutting down.")
 
 
