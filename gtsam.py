4b4c6f6dce2ca084fb4513fd8e6b60cd1c46e441
Nikhil Sridhar <niksrid@umich.edu>
2025-04-16 11:34:21 -0400

    move VO into package

--- gtsam.py
+++ gtsam.py
@@ -1,7 +1,7 @@
 import numpy as np
 import gtsam
 from typing import NamedTuple
-import visual_odometry
+import iSLAM.visual_odometry as visual_odometry
 
 # Main file to do GTSAM with Visual Odometry and IMU
 
